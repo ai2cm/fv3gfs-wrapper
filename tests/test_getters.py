3d00ca158cfed67bd3303db14cffd246064c0614
Jeremy McGibbon <jeremym@vulcan.com>
2019-10-31 16:33:39 -0700

    Fixed bug in context manager and pulled it into a separate file

--- tests/test_getters.py
+++ tests/test_getters.py
@@ -1,60 +1,15 @@
 import unittest
 import json
 import os
-import sys
 import shutil
-from contextlib import contextmanager
-import ctypes
-import io
-import tempfile
 import xarray as xr
 import numpy as np
 import fv3config
 import fv3gfs
 from mpi4py import MPI
+from util import redirect_stdout
 
 test_dir = os.path.dirname(os.path.abspath(__file__))
-
-
-libc = ctypes.CDLL(None)
-c_stdout = ctypes.c_void_p.in_dll(libc, 'stdout')
-
-
-class redirect_stdout(object):
-
-    def __init__(self, filename):
-        self.stream = open(filename, 'wb')
-        self._filename = filename
-        self._stdout_file_descriptor = sys.stdout.fileno()
-        self._saved_stdout_file_descriptor = os.dup(self._stdout_file_descriptor)
-        self._temporary_stdout = tempfile.TemporaryFile(mode='w+b')
-
-    def __enter__(self):
-        # Redirect the stdout file descriptor to point at our temporary file
-        self._redirect_stdout(self._temporary_stdout.fileno())
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        # Set the stdout file descriptor back to what it was when we started
-        self._redirect_stdout(self._saved_stdout_file_descriptor)
-        # Write contents of temporary file to the output file
-        self.temporary_stdout.flush()
-        self.temporary_stdout.seek(0, io.SEEK_SET)
-        self.stream.write(self.temporary_stdout.read())
-        # Close our temporary file and remove the duplicate file descriptor
-        self._temporary_stdout.close()
-        os.close(self._saved_stdout_file_descriptor)
-
-    def _redirect_stdout(self, to_file_descriptor):
-        """Redirect stdout to the given file descriptor."""
-        # Flush the C-level buffer stdout
-        libc.fflush(c_stdout)
-        # Flush and close sys.stdout - also closes the file descriptor (fd)
-        sys.stdout.close()
-        # Make self._stdout_file_descriptor point to the same file as to_file_descriptor
-        # This redirects stdout to the file at the file descriptor level, which C/Fortran also obeys
-        os.dup2(to_file_descriptor, self._stdout_file_descriptor)
-        # Create a new sys.stdout for Python that points to the redirected file descriptor
-        sys.stdout = io.TextIOWrapper(os.fdopen(self._stdout_file_descriptor, 'wb'))
 
 
 class GetterTests(unittest.TestCase):
