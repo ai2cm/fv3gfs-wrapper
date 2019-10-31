7e26ad2f22865ec73df5909ffb527e23d288cbef
Jeremy McGibbon <jeremym@vulcan.com>
2019-10-31 14:42:28 -0700

    silence stdout when initializing model

--- tests/test_getters.py
+++ tests/test_getters.py
@@ -3,13 +3,56 @@ import json
 import os
 import sys
 import shutil
+from contextlib import contextmanager
+import ctypes
+import io
+import tempfile
 import xarray as xr
+import numpy as np
 import fv3config
 import fv3gfs
-import numpy as np
 from mpi4py import MPI
 
 test_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+libc = ctypes.CDLL(None)
+c_stdout = ctypes.c_void_p.in_dll(libc, 'stdout')
+
+
+@contextmanager
+def redirect_stdout(filename):
+    # The original fd stdout points to. Usually 1 on POSIX systems.
+    stream = open(filename, 'wb')
+    original_stdout_fd = sys.stdout.fileno()
+
+    def _redirect_stdout(to_fd):
+        """Redirect stdout to the given file descriptor."""
+        # Flush the C-level buffer stdout
+        libc.fflush(c_stdout)
+        # Flush and close sys.stdout - also closes the file descriptor (fd)
+        sys.stdout.close()
+        # Make original_stdout_fd point to the same file as to_fd
+        os.dup2(to_fd, original_stdout_fd)
+        # Create a new sys.stdout that points to the redirected fd
+        sys.stdout = io.TextIOWrapper(os.fdopen(original_stdout_fd, 'wb'))
+
+    # Save a copy of the original stdout fd in saved_stdout_fd
+    saved_stdout_fd = os.dup(original_stdout_fd)
+    try:
+        # Create a temporary file and redirect stdout to it
+        tfile = tempfile.TemporaryFile(mode='w+b')
+        _redirect_stdout(tfile.fileno())
+        # Yield to caller, then redirect stdout back to the saved fd
+        yield
+        _redirect_stdout(saved_stdout_fd)
+        # Copy contents of temporary file to the given stream
+        tfile.flush()
+        tfile.seek(0, io.SEEK_SET)
+        stream.write(tfile.read())
+    finally:
+        tfile.close()
+        os.close(saved_stdout_fd)
 
 
 class GetterTests(unittest.TestCase):
@@ -171,7 +214,9 @@ if __name__ == '__main__':
     original_path = os.getcwd()
     os.chdir(rundir)
     try:
-        fv3gfs.initialize()
+        with redirect_stdout(os.path.join(test_dir, f'logs/test_getters.rank{rank}.log')):
+            fv3gfs.initialize()
+            MPI.COMM_WORLD.barrier()
         if rank != 0:
             kwargs = {'verbosity': 0}
         else:
--- tests/test_setters.py
+++ tests/test_setters.py
@@ -1,7 +1,12 @@
 import unittest
 import json
 import os
+import sys
 import shutil
+from contextlib import contextmanager
+import ctypes
+import io
+import tempfile
 from copy import deepcopy
 import xarray as xr
 import numpy as np
@@ -9,8 +14,45 @@ import fv3config
 import fv3gfs
 from mpi4py import MPI
 
-
 test_dir = os.path.dirname(os.path.abspath(__file__))
+
+libc = ctypes.CDLL(None)
+c_stdout = ctypes.c_void_p.in_dll(libc, 'stdout')
+
+
+@contextmanager
+def redirect_stdout(filename):
+    # The original fd stdout points to. Usually 1 on POSIX systems.
+    stream = open(filename, 'wb')
+    original_stdout_fd = sys.stdout.fileno()
+
+    def _redirect_stdout(to_fd):
+        """Redirect stdout to the given file descriptor."""
+        # Flush the C-level buffer stdout
+        libc.fflush(c_stdout)
+        # Flush and close sys.stdout - also closes the file descriptor (fd)
+        sys.stdout.close()
+        # Make original_stdout_fd point to the same file as to_fd
+        os.dup2(to_fd, original_stdout_fd)
+        # Create a new sys.stdout that points to the redirected fd
+        sys.stdout = io.TextIOWrapper(os.fdopen(original_stdout_fd, 'wb'))
+
+    # Save a copy of the original stdout fd in saved_stdout_fd
+    saved_stdout_fd = os.dup(original_stdout_fd)
+    try:
+        # Create a temporary file and redirect stdout to it
+        tfile = tempfile.TemporaryFile(mode='w+b')
+        _redirect_stdout(tfile.fileno())
+        # Yield to caller, then redirect stdout back to the saved fd
+        yield
+        _redirect_stdout(saved_stdout_fd)
+        # Copy contents of temporary file to the given stream
+        tfile.flush()
+        tfile.seek(0, io.SEEK_SET)
+        stream.write(tfile.read())
+    finally:
+        tfile.close()
+        os.close(saved_stdout_fd)
 
 
 class SetterTests(unittest.TestCase):
@@ -37,12 +79,16 @@ class SetterTests(unittest.TestCase):
 
     def test_dynamics_names_present(self):
         """Test that some small subset of dynamics names are in the data dictionary"""
-        for name in ['eastward_wind', 'northward_wind', 'vertical_wind', 'surface_geopotential']:
+        for name in [
+                'eastward_wind', 'northward_wind', 'vertical_wind',
+                'surface_geopotential']:
             self.assertIn(name, self.dynamics_data.keys())
 
     def test_physics_names_present(self):
         """Test that some small subset of physics names are in the data dictionary"""
-        for name in ['land_sea_mask', 'surface_temperature', 'surface_roughness', 'air_temperature_at_2m']:
+        for name in [
+                'land_sea_mask', 'surface_temperature', 'surface_roughness',
+                'air_temperature_at_2m']:
             self.assertIn(name, self.physics_data.keys())
 
     def test_set_then_get_cloud_amount(self):
@@ -76,10 +122,18 @@ class SetterTests(unittest.TestCase):
         self._set_names_helper(self.tracer_data.keys())
 
     def test_set_then_get_all_names(self):
-        self._set_names_helper(list(self.dynamics_data.keys()) + list(self.physics_data.keys()) + list(self.tracer_data.keys()))
+        self._set_names_helper(
+            list(self.dynamics_data.keys()) +
+            list(self.physics_data.keys()) +
+            list(self.tracer_data.keys())
+        )
 
     def test_set_then_get_only_some_names(self):
-        all_name_list = list(self.dynamics_data.keys()) + list(self.physics_data.keys()) + list(self.tracer_data.keys())
+        all_name_list = (list(
+            self.dynamics_data.keys()) +
+            list(self.physics_data.keys()) +
+            list(self.tracer_data.keys())
+        )
         self._set_names_helper(all_name_list[::3])
 
     def _set_names_helper(self, name_list):
@@ -147,7 +201,9 @@ if __name__ == '__main__':
     original_path = os.getcwd()
     os.chdir(rundir)
     try:
-        fv3gfs.initialize()
+        with redirect_stdout(os.path.join(test_dir, f'logs/test_setters.rank{rank}.log')):
+            fv3gfs.initialize()
+            MPI.COMM_WORLD.barrier()
         if rank != 0:
             kwargs = {'verbosity': 0}
         else:
