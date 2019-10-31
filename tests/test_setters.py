83519c841cd78c83226b72a5dd339b3458c3acdf
Jeremy McGibbon <jeremym@vulcan.com>
2019-10-31 14:46:08 -0700

    Made redirect context manager more verbose

--- tests/test_setters.py
+++ tests/test_setters.py
@@ -24,35 +24,35 @@ c_stdout = ctypes.c_void_p.in_dll(libc, 'stdout')
 def redirect_stdout(filename):
     # The original fd stdout points to. Usually 1 on POSIX systems.
     stream = open(filename, 'wb')
-    original_stdout_fd = sys.stdout.fileno()
+    original_stdout_file_descriptor = sys.stdout.fileno()
 
-    def _redirect_stdout(to_fd):
+    def _redirect_stdout(to_file_descriptor):
         """Redirect stdout to the given file descriptor."""
         # Flush the C-level buffer stdout
         libc.fflush(c_stdout)
         # Flush and close sys.stdout - also closes the file descriptor (fd)
         sys.stdout.close()
-        # Make original_stdout_fd point to the same file as to_fd
-        os.dup2(to_fd, original_stdout_fd)
+        # Make original_stdout_file_descriptor point to the same file as to_file_descriptor
+        os.dup2(to_file_descriptor, original_stdout_file_descriptor)
         # Create a new sys.stdout that points to the redirected fd
-        sys.stdout = io.TextIOWrapper(os.fdopen(original_stdout_fd, 'wb'))
+        sys.stdout = io.TextIOWrapper(os.fdopen(original_stdout_file_descriptor, 'wb'))
 
-    # Save a copy of the original stdout fd in saved_stdout_fd
-    saved_stdout_fd = os.dup(original_stdout_fd)
+    # Save a copy of the original stdout fd in saved_stdout_file_descriptor
+    saved_stdout_file_descriptor = os.dup(original_stdout_file_descriptor)
     try:
         # Create a temporary file and redirect stdout to it
-        tfile = tempfile.TemporaryFile(mode='w+b')
-        _redirect_stdout(tfile.fileno())
+        temporary_stdout = tempfile.TemporaryFile(mode='w+b')
+        _redirect_stdout(temporary_stdout.fileno())
         # Yield to caller, then redirect stdout back to the saved fd
         yield
-        _redirect_stdout(saved_stdout_fd)
+        _redirect_stdout(saved_stdout_file_descriptor)
         # Copy contents of temporary file to the given stream
-        tfile.flush()
-        tfile.seek(0, io.SEEK_SET)
-        stream.write(tfile.read())
+        temporary_stdout.flush()
+        temporary_stdout.seek(0, io.SEEK_SET)
+        stream.write(temporary_stdout.read())
     finally:
-        tfile.close()
-        os.close(saved_stdout_fd)
+        temporary_stdout.close()
+        os.close(saved_stdout_file_descriptor)
 
 
 class SetterTests(unittest.TestCase):
