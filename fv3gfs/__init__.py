18e3a94b9a7cc133e30266de6f30e9f081734384
Jeremy McGibbon <jeremym@vulcan.com>
2019-11-02 14:23:19 -0700

    Moved ghost cell routines to their own file

--- fv3gfs/__init__.py
+++ fv3gfs/__init__.py
@@ -2,16 +2,19 @@ from ._wrapper import (
     initialize, step, step_dynamics, step_physics, save_intermediate_restart_if_enabled,
     save_intermediate_restart,
     cleanup, get_state, set_state,
-    get_n_ghost_cells, get_step_count, without_ghost_cells, get_tracer_metadata
+    get_n_ghost_cells, get_step_count, get_tracer_metadata
 )
+from ._ghost_cells import without_ghost_cells, with_ghost_cells
 from ._exceptions import InvalidQuantityError
 from ._fortran_info import physics_properties, dynamics_properties
 from ._restart import get_restart_data
 
 __all__ = [
     'initialize', 'step', 'step_dynamics', 'step_physics', 'save_intermediate_restart_if_enabled',
+    'save_intermediate_restart',
     'cleanup', 'get_state', 'set_state',
-    'get_n_ghost_cells', 'get_step_count', 'without_ghost_cells', 'get_tracer_metadata',
+    'get_n_ghost_cells', 'get_step_count', 'get_tracer_metadata',
+    'without_ghost_cells', 'with_ghost_cells',
     'InvalidQuantityError',
     'physics_properties', 'dynamics_properties',
     'get_restart_data',
