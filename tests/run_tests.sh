18ed2db4b12d77a8b187892e2fe305c5c6794bd0
Jeremy McGibbon <jeremym@vulcan.com>
2019-11-04 09:33:26 -0800

    Re-enable tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ulimit -s unlimited
-# python3 ./test_integration.py || exit 1
-# python3 ./test_get_tile_number.py || exit 1
+python3 ./test_integration.py || exit 1
+python3 ./test_get_tile_number.py || exit 1
 mpirun -n 6 --allow-run-as-root --oversubscribe --mca btl_vader_single_copy_mechanism none python3 ./test_getters.py || exit 1
 mpirun -n 6 --allow-run-as-root --oversubscribe --mca btl_vader_single_copy_mechanism none python3 ./test_setters.py || exit 1
