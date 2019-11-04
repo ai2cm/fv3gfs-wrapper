02d96ab8f2dbcee12bda1d496cedffcc6877e031
Jeremy McGibbon <jeremym@vulcan.com>
2019-11-04 11:52:32 -0800

    Silence model stdout for tests (#9)
    * silence stdout
    
    * silence stdout when initializing model
    
    * Made redirect context manager more verbose
    
    * wrote object-oriented context manager for stdout redirect
    
    * Fixed bug in context manager and pulled it into a separate file
    
    * Added docstring to redirect_stdout
    
    * Added example to stdout redirector
    
    * Re-enable tests

---
+++ tests/util.py
@@ -0,0 +1,62 @@
+import sys
+import os
+import tempfile
+import io
+import ctypes
+
+libc = ctypes.CDLL(None)
+c_stdout = ctypes.c_void_p.in_dll(libc, 'stdout')
+
+
+def redirect_stdout(filename):
+    """
+    Context manager for temporarily redirecting sys.stdout to another file.
+
+    Behaves similarly to `contextlib.redirect_stdout`, but will also apply the
+    redirection to code in compiled shared libraries.
+
+    Usage:
+        with redirect_stdout(log_filename):
+            # output will be redirected to log file
+            do_stuff()
+    """
+    return StdoutRedirector(filename)
+
+
+class StdoutRedirector(object):
+    
+    __doc__ = redirect_stdout.__doc__
+
+    def __init__(self, filename):
+        self.stream = open(filename, 'wb')
+        self._filename = filename
+        self._stdout_file_descriptor = sys.stdout.fileno()
+        self._saved_stdout_file_descriptor = os.dup(self._stdout_file_descriptor)
+        self._temporary_stdout = tempfile.TemporaryFile(mode='w+b')
+
+    def __enter__(self):
+        # Redirect the stdout file descriptor to point at our temporary file
+        self._redirect_stdout(self._temporary_stdout.fileno())
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        # Set the stdout file descriptor back to what it was when we started
+        self._redirect_stdout(self._saved_stdout_file_descriptor)
+        # Write contents of temporary file to the output file
+        self._temporary_stdout.flush()
+        self._temporary_stdout.seek(0, io.SEEK_SET)
+        self.stream.write(self._temporary_stdout.read())
+        # Close our temporary file and remove the duplicate file descriptor
+        self._temporary_stdout.close()
+        os.close(self._saved_stdout_file_descriptor)
+
+    def _redirect_stdout(self, to_file_descriptor):
+        """Redirect stdout to the given file descriptor."""
+        # Flush the C-level buffer stdout
+        libc.fflush(c_stdout)
+        # Flush and close sys.stdout - also closes the file descriptor (fd)
+        sys.stdout.close()
+        # Make self._stdout_file_descriptor point to the same file as to_file_descriptor
+        # This redirects stdout to the file at the file descriptor level, which C/Fortran also obeys
+        os.dup2(to_file_descriptor, self._stdout_file_descriptor)
+        # Create a new sys.stdout for Python that points to the redirected file descriptor
+        sys.stdout = io.TextIOWrapper(os.fdopen(self._stdout_file_descriptor, 'wb'))
