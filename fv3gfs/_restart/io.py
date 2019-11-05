38f74faa6c30d20dd5b31c29bda2639097d0c6ed
Jeremy McGibbon <jeremym@vulcan.com>
2019-11-05 15:50:34 -0800

    Remove stochastic convective variables from restart files

--- fv3gfs/_restart/io.py
+++ fv3gfs/_restart/io.py
@@ -4,6 +4,14 @@ import numpy as np
 from .._fortran_info import dynamics_properties, physics_properties
 from .._wrapper import get_tracer_metadata
 
+# these variables are found not to be needed for smooth restarts
+# later we could represent this as a key in the dynamics/physics_properties
+RESTART_EXCLUDE_NAMES = [
+    'convective_cloud_fraction',
+    'convective_cloud_top_pressure',
+    'convective_cloud_bottom_pressure',
+]
+
 
 def get_restart_names():
     """Return a list of variables names needed for a clean restart.
@@ -11,7 +19,11 @@ def get_restart_names():
     dynamics_names = [p['name'] for p in dynamics_properties]
     physics_names = [p['name'] for p in physics_properties]
     tracer_names = list(get_tracer_metadata().keys())
-    return ['time'] + dynamics_names + tracer_names + physics_names
+    return_list = ['time'] + dynamics_names + tracer_names + physics_names
+    for name in RESTART_EXCLUDE_NAMES:
+        if name in return_list:
+            return_list.remove(name)
+    return return_list
 
 
 def datetime64_to_datetime(dt64):
