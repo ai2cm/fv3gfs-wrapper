6b881f392f4fdce632461e91308266cda9cb87ab
Jeremy McGibbon <jeremym@vulcan.com>
2019-11-06 13:30:45 -0800

    Enable testing on CircleCI

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-DOCKER_IMAGE=fv3gfs-python
+if [ -z "$DOCKER_IMAGE" ]; then
+    DOCKER_IMAGE = fv3gfs-python
+done
 
-docker build -f docker/Dockerfile -t $DOCKER_IMAGE .
+for arg in $(< $DOCKER_BUILD_ARGS); do
+    build_arg = $build_arg --build-arg $arg
+done
+
+docker build -f docker/Dockerfile -t $DOCKER_IMAGE $build_arg .
