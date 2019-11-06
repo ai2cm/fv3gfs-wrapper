9df17dea13a6b5b5ac428de89ad10b910636bdd7
Jeremy McGibbon <jeremym@vulcan.com>
2019-11-06 14:04:18 -0800

    pull submodules on circleci

--- build_docker.sh
+++ build_docker.sh
@@ -4,8 +4,10 @@ if [ -z "$DOCKER_IMAGE" ]; then
     DOCKER_IMAGE = fv3gfs-python
 fi
 
-for arg in $(< $DOCKER_BUILD_ARGS); do
-    build_arg = $build_arg --build-arg $arg
-done
+if [ ! -z "$DOCKER_BUILD_ARGS"]; then
+    for arg in $(< $DOCKER_BUILD_ARGS); do
+        build_arg = $build_arg --build-arg $arg
+    done
+fi
 
 docker build -f docker/Dockerfile -t $DOCKER_IMAGE $build_arg .
