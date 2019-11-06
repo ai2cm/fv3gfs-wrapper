41c07cf6e3f3d39d9aa3700555496f630275d814
Jeremy McGibbon <jeremym@vulcan.com>
2019-11-05 16:27:54 -0800

    Make script clearer

--- tests/integration_scripts/test_legacy_restart.py
+++ tests/integration_scripts/test_legacy_restart.py
@@ -49,27 +49,32 @@ def get_timestamp(dt):
     return f'{dt.year:04}{dt.month:02}{dt.day:02}.{dt.hour:02}{dt.minute:02}{dt.second:02}'
 
 
-if __name__ == '__main__':
-    num_steps = 2
-    fv3gfs.initialize()
+def step_model(n_steps):
     for i in range(num_steps):
         print(f'Step {i}')
         fv3gfs.step_dynamics()
         fv3gfs.step_physics()
     fv3gfs.save_fortran_restart()
     comm.barrier()
+
+
+if __name__ == '__main__':
+    num_steps = 2
+    fv3gfs.initialize()
+
+    # Step to checkpoint
+    step_model(n_steps=num_steps)
     checkpoint_data = fv3gfs.get_state(fv3gfs.get_restart_names())
     checkpoint_data_disk = fv3gfs.load_fortran_restart_folder(
         os.path.join(os.getcwd(), 'RESTART'), label=get_timestamp(checkpoint_data['time'])
     )
     test_data_equal(checkpoint_data, checkpoint_data_disk, test_case='checkpoint memory vs disk')
-    for i in range(num_steps):
-        print(f'Step {i}')
-        fv3gfs.step_dynamics()
-        fv3gfs.step_physics()
-    fv3gfs.save_fortran_restart()
-    comm.barrier()
+
+    # Step to end of run, continuously
+    step_model(n_steps=num_steps)
     first_time_data = fv3gfs.get_state(fv3gfs.get_restart_names())
+
+    # Return to checkpoint
     fv3gfs.set_state(checkpoint_data)
     comm.barrier()
     logging.info('comparing checkpoint to restart output after resetting to checkpoint')
@@ -77,11 +82,11 @@ if __name__ == '__main__':
         os.path.join(os.getcwd(), 'RESTART'), label=get_timestamp(first_time_data['time'])
     )
     test_data_equal(checkpoint_data, checkpoint_reset_data, test_case='checkpoint reset')
-    for i in range(num_steps):
-        print(f'Step {i}')
-        fv3gfs.step_dynamics()
-        fv3gfs.step_physics()
-    comm.barrier()
+
+    # Step to end of run, after having restarted
+    step_model(n_steps=num_steps)
+
+    # Check continuous-run data (first time) is the same as restarted data (second time)
     second_time_data = fv3gfs.get_state(fv3gfs.get_restart_names())
     logging.info('comparing first (continuous) and second (restarted) run')
     test_data_equal(first_time_data, second_time_data, test_case='end')
