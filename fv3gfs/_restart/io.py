fdacd0f5bd326b7b964c4250b01d8f0f0d7a39a8
Jeremy McGibbon <jeremym@vulcan.com>
2019-11-08 14:33:01 -0800

    clarified how datetime64 is converted to datetime

--- fv3gfs/_restart/io.py
+++ fv3gfs/_restart/io.py
@@ -27,7 +27,8 @@ def get_restart_names():
 
 
 def datetime64_to_datetime(dt64):
-    timestamp = (dt64 - np.datetime64('1970-01-01T00:00:00Z')) / np.timedelta64(1, 's')
+    utc_start = np.datetime64(0, 's')
+    timestamp = (dt64 - utc_start) / np.timedelta64(1, 's')
     return datetime.utcfromtimestamp(timestamp)
 
 
