bae41c37352216abef767ca9314a130e89e9f121
Jeremy McGibbon <jeremym@vulcan.com>
2019-11-08 15:00:59 -0800

    Enable testing on CircleCI (#13)
    * Enable testing on CircleCI
    
    * Added GCR instructions
    
    * fixed syntax error in if statement
    
    * fix gcr image names
    
    * auth on GCR before trying to pull images
    
    * fixed typo in environment variable name
    
    * pull submodules on circleci
    
    * pull git submodules using access token
    
    * Add user to token specification
    
    * Fixed syntax error in url
    
    * use https for submodule
    
    * Update build_docker.sh
    
    Co-Authored-By: Noah D Brenowitz <nbren12@gmail.com>
    
    * Update build_docker.sh
    
    Co-Authored-By: Noah D Brenowitz <nbren12@gmail.com>
    
    * remove bad flag from submodule update

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-DOCKER_IMAGE=fv3gfs-python
+if [ -z "$DOCKER_IMAGE" ]; then
+    DOCKER_IMAGE=fv3gfs-python
+fi
 
-docker build -f docker/Dockerfile -t $DOCKER_IMAGE .
+if [ ! -z "$DOCKER_BUILD_ARGS"]; then
+    for arg in $(< $DOCKER_BUILD_ARGS); do
+        build_arg="$build_arg --build-arg $arg"
+    done
+fi
+
+docker build -f docker/Dockerfile -t $DOCKER_IMAGE $build_arg .
