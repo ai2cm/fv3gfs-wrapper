a2ed635b4b1f7b56c15aeefe50d550597d00aa71
Jeremy McGibbon <jeremym@vulcan.com>
2019-11-20 16:14:54 -0800

    fix whitespace

--- build_docker.sh
+++ build_docker.sh
@@ -4,7 +4,7 @@ if [ -z "$DOCKER_IMAGE" ]; then
     DOCKER_IMAGE=fv3gfs-python
 fi
 
-if [ ! -z "$DOCKER_BUILD_ARGS"]; then
+if [ ! -z "$DOCKER_BUILD_ARGS" ]; then
     for arg in $(< $DOCKER_BUILD_ARGS); do
         build_arg="$build_arg --build-arg $arg"
     done
