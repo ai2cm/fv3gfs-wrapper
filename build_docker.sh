809175d88c323ba70d58f6e436e08f0c337d521b
Jeremy McGibbon <jeremym@vulcan.com>
2019-11-20 16:51:28 -0800

    use multi-stage docker build

--- build_docker.sh
+++ build_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$DOCKER_IMAGE" ]; then
-    DOCKER_IMAGE=fv3gfs-python
+    DOCKER_IMAGE=us.gcr.io/vcm-ml/fv3gfs-python
 fi
 
 if [ ! -z "$DOCKER_BUILD_ARGS" ]; then
@@ -10,4 +10,4 @@ if [ ! -z "$DOCKER_BUILD_ARGS" ]; then
     done
 fi
 
-docker build -f docker/Dockerfile -t $DOCKER_IMAGE $build_arg .
+docker build -f docker/Dockerfile -t $DOCKER_IMAGE $build_arg . --target fv3gfs-python
