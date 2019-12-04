0bcbb99e65f2d3aa9b3fa4c27c2166f4482e45ae
Jeremy McGibbon <jeremym@vulcan.com>
2019-12-04 09:52:05 -0800

    test fv3config in image also

--- test_docker.sh
+++ test_docker.sh
@@ -2,4 +2,6 @@
 
 DOCKER_IMAGE=us.gcr.io/vcm-ml/fv3gfs-python
 
-./build_docker.sh && docker run -it $DOCKER_IMAGE bash -c "cd /fv3gfs-python; make test"
+./build_docker.sh && \
+    docker run -it $DOCKER_IMAGE bash -c "cd /fv3gfs-python; make test" && \
+    docker run -it $DOCKER_IMAGE bash -c "pytest /fv3gfs-python/external/fv3config/tests"
