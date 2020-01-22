062f3803c9ce7162b2850542e9d38cae8e39f4a4
Noah D. Brenowitz <nbren12@gmail.com>
2020-01-21 17:26:30 -0800

    re-order file

--- examples/save_state/run_sklearn.py
+++ examples/save_state/run_sklearn.py
@@ -3,9 +3,11 @@ import sys
 import fsspec
 import pickle
 from sklearn.externals import joblib
+import xarray as xr
 
 memory = joblib.Memory(location='cache')
 
+SKLEARN_MODEL = "gs://vcm-ml-data/test-annak/ml-pipeline-output/2020-01-17_rf_40d_run.pkl"
 
 @memory.cache
 def open_sklearn_model(url):
@@ -13,26 +15,29 @@ def open_sklearn_model(url):
     with fsspec.open(url, "rb") as f:
         return joblib.load(f)
 
-SKLEARN_MODEL = "gs://vcm-ml-data/test-annak/ml-pipeline-output/2020-01-17_rf_40d_run.pkl"
-
-
-sys.path.insert(0, '../')
-
 
 def rename_dims(x):
     return x.rename({'z': 'pfull'})
 
+
 def rename_to_restart(state):
     return {state_io.CF_TO_RESTART_MAP.get(key, key): rename_dims(state[key]) for key in state}
+
+
+def predict(model, state):
+    state = xr.Dataset(state)
+    stacked = state.stack(sample=['x', 'y'])
+    output = model.predict(stacked, 'sample')
+    return output.unstack('sample')
 
 
 with open("rundir/state.pkl", "rb") as f:
     data = state_io.load(f)
 
 
-def predict(model, state):
-    state.stack(sample=['x', 'y'])
+
 renamed = [rename_to_restart(it) for it in data]
 tile = renamed[0]
 model = open_sklearn_model(SKLEARN_MODEL)
-
+preds = predict(model, tile)
+print(preds)
