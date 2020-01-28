2b820657af0d343c7153fd952de107aff964847e
Jeremy McGibbon <jeremym@vulcan.com>
2020-01-27 17:13:40 -0800

    pass google credentials into container when running fv3config tests

--- test_docker.sh
+++ test_docker.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+set -e
+
 DOCKER_IMAGE=us.gcr.io/vcm-ml/fv3gfs-python
 
-./build_docker.sh && \
-    docker run -it $DOCKER_IMAGE bash -c "cd /fv3gfs-python; make test" && \
+./build_docker.sh
+docker run -it $DOCKER_IMAGE bash -c "cd /fv3gfs-python; make test"
+
+if [[ "GOOGLE_APPLICATION_CREDENTIALS" == "" ]]
     docker run -it $DOCKER_IMAGE bash -c "pytest /fv3gfs-python/external/fv3config/tests"
+else  # needed for circleci tests on machine executor, even though we're accessing public data only
+    docker run -v $GOOGLE_APPLICATION_CREDENTIALS:$GOOGLE_APPLICATION_CREDENTIALS \
+        --env GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
+        -it $DOCKER_IMAGE bash -c "pytest /fv3gfs-python/external/fv3config/tests"
+fi
