7eee7a728f3877199078302aa0187cca5b606df6
Jeremy McGibbon <jeremym@vulcan.com>
2020-01-29 11:19:11 -0800

    install fv3config for tests

--- test_docker.sh
+++ test_docker.sh
@@ -6,6 +6,8 @@ DOCKER_IMAGE=us.gcr.io/vcm-ml/fv3gfs-python
 
 ./build_docker.sh
 
+pytest ./tests/image_tests/*.py
+
 if [[ "GOOGLE_APPLICATION_CREDENTIALS" == "" ]]
 then
     docker run -it $DOCKER_IMAGE bash -c "pytest /fv3gfs-python/external/fv3config/tests"
@@ -19,5 +21,3 @@ else
         --env GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
         -it $DOCKER_IMAGE bash -c "cd /fv3gfs-python; make test"
 fi
-
-pytest ./tests/image_tests/*.py
