7fe8b206c8106bea6607095f64d7a7e506528ea6
Jeremy McGibbon <jeremym@vulcan.com>
2020-03-06 13:48:39 -0800

    add constants and UnitsError to top-level namespace

--- fv3gfs/__init__.py
+++ fv3gfs/__init__.py
@@ -9,7 +9,8 @@ from fv3util import (
     InvalidQuantityError, DYNAMICS_PROPERTIES, PHYSICS_PROPERTIES,
     read_state, write_state, apply_nudging, get_nudging_tendencies, ZarrMonitor,
     CubedSpherePartitioner, TilePartitioner, CubedSphereCommunicator, TileCommunicator,
-    Quantity
+    Quantity, X_DIMS, Y_DIMS, HORIZONTAL_DIMS, INTERFACE_DIMS, X_DIM, X_INTERFACE_DIM,
+    Y_DIM, Y_INTERFACE_DIM, Z_DIM, Z_INTERFACE_DIM, UnitsError
 )
 
 
@@ -23,7 +24,8 @@ __all__ = [
     'read_state', 'write_state', 'get_restart_names',
     'apply_nudging', 'get_nudging_tendencies', 'open_restart', 'ZarrMonitor',
     'CubedSpherePartitioner', 'TilePartitioner', 'CubedSphereCommunicator', 'TileCommunicator',
-    'Quantity',
+    'Quantity', 'X_DIMS', 'Y_DIMS', 'HORIZONTAL_DIMS', 'INTERFACE_DIMS', 'X_DIM', 'X_INTERFACE_DIM',
+    'Y_DIM', 'Y_INTERFACE_DIM', 'Z_DIM', 'Z_INTERFACE_DIM', 'UnitsError'
 ]
 
 __version__ = '0.3.0'
