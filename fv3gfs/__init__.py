ed8b03d59409c24df5f6ca422b9dcfb9734d4bea
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-03-08 00:47:17 +0000

    Add update func to fv3gfs.__init__

--- fv3gfs/__init__.py
+++ fv3gfs/__init__.py
@@ -1,6 +1,6 @@
 from ._wrapper import (
     initialize, step, step_dynamics, step_physics, save_intermediate_restart_if_enabled,
-    save_fortran_restart,
+    save_fortran_restart, update_dgrid_winds_with_agrid_tendencies 
     cleanup, get_state, set_state,
     get_n_ghost_cells, get_step_count, get_tracer_metadata
 )
@@ -15,7 +15,7 @@ from fv3util import (
 
 __all__ = [
     'initialize', 'step', 'step_dynamics', 'step_physics', 'save_intermediate_restart_if_enabled',
-    'save_fortran_restart',
+    'save_fortran_restart', 'update_dgrid_winds_with_agrid_tendencies',
     'cleanup', 'get_state', 'set_state',
     'get_n_ghost_cells', 'get_step_count', 'get_tracer_metadata',
     'without_ghost_cells', 'with_ghost_cells',
