92cc5736ee7807698782fe1808616c7ab3b74e17
Jeremy McGibbon <jeremym@vulcan.com>
2020-03-18 09:18:00 -0700

    working mpi test framework based on using mpirun to call

--- external/fv3util/tests/test_mpi_mock.py
+++ external/fv3util/tests/test_mpi_mock.py
@@ -9,29 +9,28 @@ try:
 except ImportError:
     MPI = None
 
-
-def get_mpi():
-    try:
-        from mpi4py import MPI
-    except ImportError:
-        MPI = None
-    return MPI
+if MPI is not None and MPI.COMM_WORLD.Get_size() == 1:
+    # not run as a parallel test, disable MPI tests
+    MPI.Finalize()
+    MPI = None
 
 
-def worker_setup():
-    MPI = get_mpi()
-    return MPI.Comm.Get_parent()
+def return_worker(comm):
+    return 1
 
 
-@pytest.fixture
+def send_worker(comm):
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    comm.
+
+
+@pytest.fixture(params=['return_worker', 'send_worker'])
 def worker_function():
-    def worker(comm):
-        return 1
-    return worker
-    
+    return globals()[request.param]
 
-@pytest.fixture
-def parent_function(worker_function):
+
+def gather_decorator(worker_function):
     def wrapped(comm):
         result = worker_function(comm)
         return comm.gather(result, root=0)
@@ -58,34 +57,12 @@ def dummy_list(total_ranks):
 
 @pytest.fixture
 def comm(worker_function, total_ranks):
-    with tempfile.NamedTemporaryFile(mode='w', suffix='.py') as worker_script:
-        worker_script.write(
-            inspect.getsource(get_mpi) +
-            inspect.getsource(worker_setup) +
-            inspect.getsource(worker_function) +
-            f"""
-print('setting up')
-comm = worker_setup()
-print('computing result')
-result = {worker_function.__name__}(comm)
-print('gather')
-comm.gather(result, root=0)
-comm.Disconnect()
-            """
-        )
-        comm = MPI.COMM_SELF.Spawn(
-            sys.executable,
-            args=[worker_script.name],
-            maxprocs=total_ranks
-        )
-        yield comm
-        comm.Disconnect()
+    return MPI.COMM_WORLD
 
 
 @pytest.fixture
-def mpi_results(comm, parent_function):
-    print('parent rank', comm.Get_rank())
-    return parent_function(comm)
+def mpi_results(comm, worker_function):
+    return gather_decorator(worker_function)(comm)
 
 
 @pytest.fixture
@@ -96,10 +73,14 @@ def dummy_results(worker_function, dummy_list):
     return result_list
 
 
-@pytest.mark.skipif(MPI is None, reason='mpi4py is not available')
-def test_worker(dummy_results, mpi_results):
-    for dummy_result, mpi_result in zip(dummy_results, mpi_results):
-        if isinstance(dummy_result, np.ndarray):
-            np.testing.assert_array_equal(dummy_result, mpi_result)
-        else:
-            assert dummy_result == mpi_result
+@pytest.mark.skipif(
+    MPI is None, reason='mpi4py is not available or pytest was not run in parallel'
+)
+def test_worker(comm, dummy_results, mpi_results):
+    comm.barrier()
+    if comm.Get_rank() == 0:
+        for dummy_result, mpi_result in zip(dummy_results, mpi_results):
+            if isinstance(dummy_result, np.ndarray):
+                np.testing.assert_array_equal(dummy_result, mpi_result)
+            else:
+                assert dummy_result == mpi_result
