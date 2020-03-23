2dabbfef098a10551cfd5b503a1a8b2e3cefce42
Jeremy McGibbon <jeremym@vulcan.com>
2020-03-23 16:01:43 -0700

    fix scalar rotation to work for serialized halo update test

--- external/fv3util/fv3util/communicator.py
+++ external/fv3util/fv3util/communicator.py
@@ -286,9 +286,9 @@ def rotate_scalar_data(data, dims, numpy, n_clockwise_rotations):
                 raise NotImplementedError(
                     "cannot yet rotate values which don't have both x and y dims")
             elif n_clockwise_rotations == 1:
-                data = numpy.rot90(data, axes=(y_dim, x_dim))
-            elif n_clockwise_rotations == 3:
                 data = numpy.rot90(data, axes=(x_dim, y_dim))
+            elif n_clockwise_rotations == 3:
+                data = numpy.rot90(data, axes=(y_dim, x_dim))
     elif n_clockwise_rotations == 2:
         slice_list = []
         for dim in dims:
