fix rotation tests while keeping halo update code working

external/fv3util/fv3util/communicator.py
@@ -3,6 +3,7 @@ from .quantity import Quantity, QuantityMetadata
 from .partitioner import CubedSpherePartitioner, TilePartitioner
 from . import constants
 from .boundary import Boundary
+from .rotate import rotate_scalar_data, rotate_vector_data
 import logging
 
 __all__ = ['TileCommunicator', 'CubedSphereCommunicator']
@@ -215,8 +216,12 @@ class CubedSphereCommunicator(Communicator):
             raise ValueError('cannot perform a halo update on zero halo points')
         for boundary_type, boundary in self.boundaries.items():
             data = boundary.send_view(quantity, n_points=n_points)
+            # sending data across the boundary will rotate the data
+            # n_clockwise_rotations times, due to the difference in axis orientation.\
+            # Thus we rotate that number of times counterclockwise before sending,
+            # to get the right final orientation
             data = quantity.np.ascontiguousarray(
-                rotate_scalar_data(data, quantity.dims, quantity.np, boundary.n_clockwise_rotations)
+                rotate_scalar_data(data, quantity.dims, quantity.np, -boundary.n_clockwise_rotations)
             )
             self.comm.Isend(data, dest=boundary.to_rank)
 
@@ -243,7 +248,7 @@ class CubedSphereCommunicator(Communicator):
             logger.debug("%s %s", x_data.shape, y_data.shape)
             x_data, y_data = rotate_vector_data(
                 x_data, y_data,
-                boundary.n_clockwise_rotations,
+                -boundary.n_clockwise_rotations,
                 x_quantity.dims, x_quantity.np
             )
             # x_data = x_quantity.np.ascontiguousarray(x_data)
@@ -268,50 +273,3 @@ class CubedSphereCommunicator(Communicator):
             self.finish_halo_update(x_quantity, n_points, tag=tag)
             logger.debug('finish_vector_halo_update: retrieving y quantity on rank=%i', self.rank)
             self.finish_halo_update(y_quantity, n_points, tag=tag)
-
-
-def rotate_scalar_data(data, dims, numpy, n_clockwise_rotations):
-    n_clockwise_rotations = n_clockwise_rotations % 4
-    if n_clockwise_rotations == 0:
-        pass
-    elif n_clockwise_rotations in (1, 3):
-        x_dim, y_dim = None, None
-        for i, dim in enumerate(dims):
-            if dim in constants.X_DIMS:
-                x_dim = i
-            elif dim in constants.Y_DIMS:
-                y_dim = i
-        if (x_dim is not None) or (y_dim is not None):
-            if x_dim is None or y_dim is None:
-                raise NotImplementedError(
-                    "cannot yet rotate values which don't have both x and y dims")
-            elif n_clockwise_rotations == 1:
-                data = numpy.rot90(data, axes=(x_dim, y_dim))
-            elif n_clockwise_rotations == 3:
-                data = numpy.rot90(data, axes=(y_dim, x_dim))
-    elif n_clockwise_rotations == 2:
-        slice_list = []
-        for dim in dims:
-            if dim in constants.HORIZONTAL_DIMS:
-                slice_list.append(slice(None, None, -1))
-            else:
-                slice_list.append(slice(None, None))
-        data = data[slice_list]
-    return data
-
-
-def rotate_vector_data(x_data, y_data, n_clockwise_rotations, dims, numpy):
-    x_data = rotate_scalar_data(x_data, dims, numpy, n_clockwise_rotations)
-    y_data = rotate_scalar_data(y_data, dims, numpy, n_clockwise_rotations)
-    data = [x_data, y_data]
-    n_clockwise_rotations = n_clockwise_rotations % 4
-    if n_clockwise_rotations == 0:
-        pass
-    elif n_clockwise_rotations == 3:
-        data[0], data[1] = data[1], -data[0]
-    elif n_clockwise_rotations == 2:
-        data[0], data[1] = -data[0], -data[1]
-    elif n_clockwise_rotations == 1:
-        data[0], data[1] = -data[1], data[0]
-    return data
-

external/fv3util/tests/test_rotate.py
@@ -1,4 +1,4 @@
-import fv3util
+import fv3util.rotate
 import pytest
 import numpy as np
 
@@ -75,7 +75,7 @@ import numpy as np
 )
 def test_rotate_scalar_data(
         start_data, n_clockwise_rotations, dims, target_data):
-    result = fv3util.communicator.rotate_scalar_data(
+    result = fv3util.rotate.rotate_scalar_data(
         start_data, dims, np, n_clockwise_rotations
     )
     np.testing.assert_array_equal(result, target_data)
@@ -132,7 +132,7 @@ def test_rotate_vector_data(
         start_data, n_clockwise_rotations, dims, target_data):
     x_data, y_data = start_data
     x_target, y_target = target_data
-    x_result, y_result = fv3util.communicator.rotate_vector_data(
+    x_result, y_result = fv3util.rotate.rotate_vector_data(
         x_data, y_data, n_clockwise_rotations, dims, np
     )
     np.testing.assert_array_equal(x_result, x_target)