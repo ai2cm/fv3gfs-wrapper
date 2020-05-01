8df8b6dd9bcfad86a283ce9acaf9be6d0ecc56d6
Noah D. Brenowitz <nbren12@gmail.com>
2020-04-30 17:59:37 -0700

    fix tests

--- external/fv3util/fv3util/_logging.py
+++ external/fv3util/fv3util/_logging.py
@@ -5,13 +5,13 @@ import os
 
 
 @contextlib.contextmanager
-def capture_stream(stream, logger_name="fv3gfs"):
+def capture_stream(stream, logger=None):
 
     # parent process:
     # close the reading end, we won't need this
+    orig_file_handle = os.dup(stream.fileno())
     with tempfile.TemporaryFile() as out:
         try:
-            orig_file_handle = os.dup(stream.fileno())
             # overwrite the streams fileno with a the pipe to be read by the forked
             # process below
             os.dup2(out.fileno(), stream.fileno())
@@ -21,7 +21,7 @@ def capture_stream(stream, logger_name="fv3gfs"):
             os.dup2(orig_file_handle, stream.fileno())
 
             # print logging info
-            logger = logging.getLogger(logger_name)
+            logger = logging.getLogger(__file__) if logger is None else logger
             out.seek(0)
             for line in out:
                 logger.debug(line.strip().decode("UTF-8"))
--- external/fv3util/tests/test_logging.py
+++ external/fv3util/tests/test_logging.py
@@ -1,12 +1,24 @@
-from fv3util import capture_stream
-import logging
 import sys
+from unittest.mock import Mock
+import pytest
+import os
+import ctypes
+
+from fv3util import capture_stream
 
 
-def test_capture_stream():
-    logging.basicConfig(level=logging.DEBUG)
+def get_libc():
+    if os.uname().sysname == 'Linux':
+        return ctypes.cdll.LoadLibrary("libc.so.6")
+    else:
+        pytest.skip()
 
-    with capture_stream(sys.stdout):
-        print("should appear")
-        print("should appear")
-        print("should appear")
+
+def test_capture_stream_python_print():
+    logger =  Mock()
+    text = "hello world"
+
+    with capture_stream(sys.stdout, logger=logger):
+        print(text)
+
+    logger.debug.assert_called_with(text)
