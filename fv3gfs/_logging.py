044f50baf8d2d16f4643fb31302d6e60a7784fd7
Noah D. Brenowitz <nbren12@gmail.com>
2020-04-30 22:48:41 -0700

    lint

--- fv3gfs/_logging.py
+++ fv3gfs/_logging.py
@@ -6,6 +6,8 @@ from mpi4py import MPI
 
 logger = logging.getLogger(f"fv3gfs({MPI.COMM_WORLD.rank})")
 
+CAPTURE = True
+
 
 def _log_bytes(b):
     for line in b.decode("UTF-8").split("\n"):
@@ -24,10 +26,34 @@ def _log_bytes(b):
 def _captured_stream(func):
     @wraps(func)
     def myfunc(*args, **kwargs):
-        with capture_stream(sys.stdout) as out_io, capture_stream(sys.stderr) as err_io:
+        if CAPTURE:
+            with capture_stream(sys.stdout) as out_io, capture_stream(
+                sys.stderr
+            ) as err_io:
+                out = func(*args, **kwargs)
+            _log_bytes(out_io.getvalue())
+            _log_bytes(err_io.getvalue())
+        else:
             out = func(*args, **kwargs)
-        _log_bytes(out_io.getvalue())
-        _log_bytes(err_io.getvalue())
         return out
 
     return myfunc
+
+
+def capture_fortran_output():
+    """Turns on the fortran output capturing feature
+
+    See ``show_fortran_output``.
+    """
+    # capture stderr and stdout of fortran code
+    global CAPTURE
+    CAPTURE = True
+
+
+def show_fortran_output():
+    """Disables the fortran output capturing feature
+    
+    See ``capture_fortran_output``.
+    """
+    global CAPTURE
+    CAPTURE = False
