c0b55c4ff7f6c2ae4a1de3ae5add69fc94e8da92
Noah D. Brenowitz <nbren12@gmail.com>
2020-07-28 14:25:40 -0700

    Remove tests moved to fv3util

--- test_docker.sh
+++ test_docker.sh
@@ -21,9 +21,6 @@ else
 # needed for circleci tests on machine executor, even though we're accessing public data only
     docker run -v $GOOGLE_APPLICATION_CREDENTIALS:$GOOGLE_APPLICATION_CREDENTIALS \
         --env GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
-        -it $DOCKER_IMAGE bash -c "make -C /fv3gfs-python/external/fv3util test test_mpi"
-    docker run -v $GOOGLE_APPLICATION_CREDENTIALS:$GOOGLE_APPLICATION_CREDENTIALS \
-        --env GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
         -it $DOCKER_IMAGE bash -c "cd /fv3gfs-python; make test"
     docker run -v $GOOGLE_APPLICATION_CREDENTIALS:$GOOGLE_APPLICATION_CREDENTIALS \
         --env GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
