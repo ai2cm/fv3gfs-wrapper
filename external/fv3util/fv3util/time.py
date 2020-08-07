77683c06578bea552f3397d9ce93377ec4cddb99
Spencer Clark <spencerkclark@gmail.com>
2020-08-07 16:42:53 +0000

    reformat

--- external/fv3util/fv3util/time.py
+++ external/fv3util/fv3util/time.py
@@ -20,4 +20,3 @@ def datetime64_to_datetime(dt64):
     utc_start = np.datetime64(0, "s")
     timestamp = (dt64 - utc_start) / np.timedelta64(1, "s")
     return datetime.utcfromtimestamp(timestamp)
-
