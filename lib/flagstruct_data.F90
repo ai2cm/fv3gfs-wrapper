f6a626ac0cba5507784f8e130ae1e50c65cdf4fd
oelbert <olivere@vulcan.com>
2020-08-10 16:39:58 -0400

    this version builds, but has the flagstruct code commented out

---
+++ lib/flagstruct_data.F90
@@ -0,0 +1,58 @@
+module flagstruct_data_mod
+
+    use atmosphere_mod, only: Atm, mytile
+    use fv_nwp_nudge_mod, only: do_adiabatic_init
+    !use tracer_manager_mod, only: get_tracer_names, get_number_tracers, get_tracer_index
+    !use field_manager_mod,  only: MODEL_ATMOS
+    use iso_c_binding
+    
+    implicit none
+    
+    contains
+    
+
+    
+    subroutine get_n_split(n_split_out) bind(c)
+        integer(c_int), intent(out) :: n_split_out
+        n_split_out = Atm(mytile)%flagstruct%n_split
+    end subroutine get_n_split
+    
+
+    
+    subroutine get_consv_te(consv_te_out) bind(c)
+        real(c_double), intent(out) :: consv_te_out
+        consv_te_out = Atm(mytile)%flagstruct%consv_te
+    end subroutine get_consv_te
+    
+
+    
+    subroutine get_dt_atmos(dt_atmos_out) bind(c)
+        integer(c_int), intent(out) :: dt_atmos_out
+        dt_atmos_out = Atm(mytile)%flagstruct%dt_atmos
+    end subroutine get_dt_atmos
+    
+
+    
+    subroutine get_ks(ks_out) bind(c)
+        integer(c_int), intent(out) :: ks_out
+        ks_out = Atm(mytile)%flagstruct%ks
+    end subroutine get_ks
+    
+
+    
+    subroutine get_ptop(ptop_out) bind(c)
+        real(c_double), intent(out) :: ptop_out
+        ptop_out = Atm(mytile)%flagstruct%ptop
+    end subroutine get_ptop
+    
+
+    
+    subroutine get_do_adiabatic_init(do_adiabatic_init_out) bind(c)
+        logical(c_bool), intent(out) :: do_adiabatic_init_out
+        do_adiabatic_init_out = do_adiabatic_init
+    end subroutine get_do_adiabatic_init
+    
+
+    
+    end module flagstruct_data_mod
+    
