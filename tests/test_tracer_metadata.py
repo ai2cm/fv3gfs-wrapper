825ddb4d69c0f1b3a2ecdda5262f2f88ba3c1fff
Noah D. Brenowitz <nbren12@gmail.com>
2020-08-14 12:51:42 -0700

    Implement tests correctly

--- tests/test_tracer_metadata.py
+++ tests/test_tracer_metadata.py
@@ -32,9 +32,11 @@ class TracerMetadataTests(unittest.TestCase):
                 self.assertIn("units", metadata)
                 self.assertIn("i_tracer", metadata)
                 self.assertIn("fortran_name", metadata)
+                self.assertIn("is_water", metadata)
                 self.assertIsInstance(metadata["units"], str)
                 self.assertIsInstance(metadata["i_tracer"], int)
                 self.assertIsInstance(metadata["fortran_name"], str)
+                self.assertIsInstance(metadata["is_water"], bool)
 
     def test_all_tracers_present(self):
         tracer_names = [
@@ -49,6 +51,14 @@ class TracerMetadataTests(unittest.TestCase):
         ]
         data = fv3gfs.get_tracer_metadata()
         self.assertEqual(set(data.keys()), set(tracer_names))
+
+    def test_ozone_not_water(self):
+        data = fv3gfs.get_tracer_metadata()
+        self.assertFalse(data["ozone_mixing_ratio"]["is_water"])
+
+    def test_specific_humidity_is_water(self):
+        data = fv3gfs.get_tracer_metadata()
+        self.assertTrue(data["specific_humidity"]["is_water"])
 
 
 if __name__ == "__main__":
