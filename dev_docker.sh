414ba6a83509ad04a7a7768f637d88e0fec09a5d
Noah D. Brenowitz <nbren12@gmail.com>
2020-08-19 11:39:04 -0700

    Remove mounts from dev docker

--- dev_docker.sh
+++ dev_docker.sh
@@ -2,9 +2,6 @@
 
 IMAGE=us.gcr.io/vcm-ml/fv3gfs-python:latest
 
-MOUNTS="-v $(pwd)/fv3gfs:/fv3gfs-python/fv3gfs \
-        -v $(pwd)/external:/fv3gfs-python/external -v $(pwd)/lib:/fv3gfs-python/lib -v $(pwd)/tests:/fv3gfs-python/tests -v $(pwd)/templates:/fv3gfs-python/templates\
-	-v $(pwd)/examples:/fv3gfs-python/examples"
 MOUNTS="-v $(pwd):/fv3gfs-python"
 
 CONF_DIR=./lib/external/FV3/conf/
