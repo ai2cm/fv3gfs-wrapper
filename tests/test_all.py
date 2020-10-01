347bf9f2e363e4ae05aa01a5f79ad4424886574e
Noah D. Brenowitz <nbren12@gmail.com>
2020-09-30 17:15:48 -0700

    Uncomment other tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -12,26 +12,26 @@ MPI.Finalize()
 
 
 class UsingMPITests(unittest.TestCase):
-    # def test_getters(self):
-    #     run_unittest_script(os.path.join(base_dir, "test_getters.py"))
+    def test_getters(self):
+        run_unittest_script(os.path.join(base_dir, "test_getters.py"))
 
-    # def test_setters(self):
-    #     run_unittest_script(os.path.join(base_dir, "test_setters.py"))
+    def test_setters(self):
+        run_unittest_script(os.path.join(base_dir, "test_setters.py"))
 
     def test_diagnostics(self):
         run_unittest_script(os.path.join(base_dir, "test_diagnostics.py"))
 
-    # def test_tracer_metadata(self):
-    #     run_unittest_script(os.path.join(base_dir, "test_tracer_metadata.py"))
+    def test_tracer_metadata(self):
+        run_unittest_script(os.path.join(base_dir, "test_tracer_metadata.py"))
 
-    # def test_get_time_julian(self):
-    #     run_unittest_script(os.path.join(base_dir, "test_get_time.py"), "julian")
+    def test_get_time_julian(self):
+        run_unittest_script(os.path.join(base_dir, "test_get_time.py"), "julian")
 
-    # def test_get_time_thirty_day(self):
-    #     run_unittest_script(os.path.join(base_dir, "test_get_time.py"), "thirty_day")
+    def test_get_time_thirty_day(self):
+        run_unittest_script(os.path.join(base_dir, "test_get_time.py"), "thirty_day")
 
-    # def test_get_time_noleap(self):
-    #     run_unittest_script(os.path.join(base_dir, "test_get_time.py"), "noleap")
+    def test_get_time_noleap(self):
+        run_unittest_script(os.path.join(base_dir, "test_get_time.py"), "noleap")
 
 
 if __name__ == "__main__":
