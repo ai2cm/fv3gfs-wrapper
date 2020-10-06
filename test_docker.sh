b03a074cb4e17c80111fe9c5ab01dab0494bfa48
Oliver Fuhrer <oliverf@vulcan.com>
2020-10-06 22:32:22 +0000

    fix test_docker.sh script

--- test_docker.sh
+++ test_docker.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-if [ -z "$DOCKER_IMAGE" ]; then
+if [ -z "$DOCKER_IMAGE" ] ; then
     DOCKER_IMAGE=us.gcr.io/vcm-ml/fv3gfs-wrapper:gnu9-mpich314-nocuda
 fi
 
 pytest ./tests/image_tests/*.py
 
-if [[ "GOOGLE_APPLICATION_CREDENTIALS" == "" ]]
-then
-    docker run -it $DOCKER_IMAGE bash -c "cd /fv3gfs-wrapper; make test"
+if [ -z "$GOOGLE_APPLICATION_CREDENTIALS" ] ; then
+    docker run -it "$DOCKER_IMAGE" bash -c "cd /fv3gfs-wrapper; make test"
 else
     docker run -v $GOOGLE_APPLICATION_CREDENTIALS:$GOOGLE_APPLICATION_CREDENTIALS \
         --env GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
