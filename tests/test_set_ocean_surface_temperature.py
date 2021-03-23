c4e34a227d1f54cb4bf150e4f6bb891555dd6f03
Spencer Clark <spencerkclark@gmail.com>
2021-03-23 19:06:47 +0000

    Fix test

--- tests/test_set_ocean_surface_temperature.py
+++ tests/test_set_ocean_surface_temperature.py
@@ -48,7 +48,7 @@ class PrescribeSSTTests(unittest.TestCase):
         )
 
         # We expect these states to differ.
-        assert not np.assert_allclose(
+        assert not np.isclose(
             air_temperature_from_default_ocean_temperature,
             air_temperature_from_prescribed_ocean_temperature,
         )
