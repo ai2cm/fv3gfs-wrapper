0d1b966ddabd9a665c38515d5b2c4dd36dc47f05
Noah D. Brenowitz <nbren12@gmail.com>
2021-04-28 13:55:11 -0700

    test_docker getter/setter tests working

--- test_docker.sh
+++ test_docker.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 set -e
+set -x
 
 if [ -z "$DOCKER_IMAGE" ]; then
-    DOCKER_IMAGE=us.gcr.io/vcm-ml/fv3gfs-wrapper:gnu7-mpich314-nocuda
+    DOCKER_IMAGE=us.gcr.io/vcm-ml/fv3gfs-wrapper
 fi
 
 pytest ./tests/image_tests/*.py
 
 if [[ "GOOGLE_APPLICATION_CREDENTIALS" == "" ]]
 then
-    docker run -it $DOCKER_IMAGE bash -c "cd /fv3gfs-wrapper; make test"
+    docker run -it $DOCKER_IMAGE make -C /fv3gfs-wrapper test
 else
     docker run -v $GOOGLE_APPLICATION_CREDENTIALS:$GOOGLE_APPLICATION_CREDENTIALS \
         --env GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
-        -it $DOCKER_IMAGE bash -c "cd /fv3gfs-wrapper; make test"
+        -it $DOCKER_IMAGE make -C fv3gfs-wrapper test
 fi
