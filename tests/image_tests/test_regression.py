e813f361b38bdfd07c3fdb3e5ab7bc1644945055
Noah D. Brenowitz <nbren12@gmail.com>
2021-04-28 14:58:39 -0700

    verify checksums against installed fv3
    The build environment of this repo may differ from that of FV3, which means
    the checksums might change too. This makes the checksums less brittle, by
    verifying the wrapper against the installed FV3.
    
    Resolves #271

--- tests/image_tests/test_regression.py
+++ tests/image_tests/test_regression.py
@@ -1,31 +1,14 @@
 import os
 import yaml
-import shutil
 import subprocess
 import pytest
 import fv3config
+import hashlib
 
 
 TEST_DIR = os.path.dirname(os.path.realpath(__file__))
-REFERENCE_DIR = os.path.join(TEST_DIR, "reference")
-OUTPUT_DIR = os.path.join(TEST_DIR, "output")
 CONFIG_DIR = os.path.join(TEST_DIR, "config")
-SUBMIT_JOB_FILENAME = os.path.join(TEST_DIR, "run_files/submit_job.sh")
-STDOUT_FILENAME = "stdout.log"
-STDERR_FILENAME = "stderr.log"
-MD5SUM_FILENAME = "md5.txt"
-
-USE_LOCAL_ARCHIVE = True
-
 config_filenames = os.listdir(CONFIG_DIR)
-
-
-@pytest.fixture
-def reference_dir(request):
-    refdir = request.config.getoption("--refdir")
-    if refdir is None:
-        refdir = os.path.join(TEST_DIR, "reference/circleci")
-    return refdir
 
 
 @pytest.fixture(params=config_filenames)
@@ -35,42 +18,62 @@ def config(request):
         return yaml.safe_load(config_file)
 
 
-def test_regression(config, reference_dir):
-    run_name = config["experiment_name"]
-    run_reference_dir = os.path.join(reference_dir, run_name)
-    run_dir = os.path.join(OUTPUT_DIR, run_name)
-    if os.path.isdir(run_dir):
-        shutil.rmtree(run_dir)
-    os.makedirs(run_dir)
-    run_model(config, run_dir)
-    md5sum_filename = os.path.join(run_reference_dir, MD5SUM_FILENAME)
-    if not os.path.isfile(md5sum_filename):
-        assert False, (
-            f"reference md5sum does not exist, "
-            f"you can create it with `bash set_reference.sh {reference_dir}`"
-            " after running the model and generating the output directories."
-        )
-    else:
-        check_md5sum(run_dir, md5sum_filename)
-    shutil.rmtree(run_dir)
+def md5_from_dir(dir_):
+    md5s = {}
+    for root, dirs, files in os.walk(str(dir_)):
+        for file in files:
+            with open(os.path.join(root, file), "rb") as f:
+                md5 = hashlib.md5()
+                while True:
+                    buf = f.read(2048)
+                    if not buf:
+                        break
+                    md5.update(buf)
+
+            md5s[file] = md5.hexdigest()
+    return md5s
 
 
-def run_model(config, run_dir):
-    fv3config.write_run_directory(config, run_dir)
+def md5_from_dir_only_nc(dir_):
+    return {
+        file: hash for file, hash in md5_from_dir(dir_).items() if file.endswith(".nc")
+    }
+
+
+def test_md5_from_dir(tmpdir):
+    tmpdir.join("a").open("w").write("hello")
+    tmpdir.join("b").open("w").write("world")
+
+    orig_md5 = md5_from_dir(tmpdir)
+    assert orig_md5 == md5_from_dir(tmpdir)
+
+    tmpdir.join("b").open("w").write("world updated")
+    assert orig_md5 != md5_from_dir(tmpdir)
+
+
+def test_regression(tmpdir, config):
+    fv3_rundir = tmpdir.join("fv3")
+    wrapper_rundir = tmpdir.join("wrapper")
+
+    run_fv3(config, fv3_rundir)
+    run_wrapper(config, wrapper_rundir)
+
+    assert md5_from_dir_only_nc(fv3_rundir) == md5_from_dir_only_nc(wrapper_rundir)
+
+    # just make sure there are some outputs
+    assert len(md5_from_dir_only_nc(fv3_rundir)) > 0
+
+
+def run_fv3(config, run_dir):
+    fv3config.write_run_directory(config, str(run_dir))
+    subprocess.check_call(
+        ["mpirun", "-n", "6", "fv3.exe"], cwd=run_dir,
+    )
+
+
+def run_wrapper(config, run_dir):
+    fv3config.write_run_directory(config, str(run_dir))
     subprocess.check_call(
         ["mpirun", "-n", "6", "python3", "-m", "mpi4py", "-m", "fv3gfs.wrapper.run"],
         cwd=run_dir,
     )
-
-
-def check_md5sum(run_dir, md5sum_filename):
-    subprocess.check_call(["md5sum", "-c", md5sum_filename], cwd=run_dir)
-
-
-def write_run_directory(config, dirname):
-    fv3config.write_run_directory(config, dirname)
-    shutil.copy(SUBMIT_JOB_FILENAME, os.path.join(dirname, "submit_job.sh"))
-
-
-if __name__ == "__main__":
-    pytest.main()
